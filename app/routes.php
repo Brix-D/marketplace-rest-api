a7459c06af3b8693224aa7ae513b1afd612cea4a
Brix-D <Shlyakhta4@gmail.com>
2021-11-05 23:27:23 +0400

    created index page

--- app/routes.php
+++ app/routes.php
@@ -1,8 +1,9 @@
 <?php
 
 use Services\Router;
+use Controllers\Index;
 
 require_once "controllers/api/Auth/routes.php";
 
-
+Router::page('/', fn() => Index::action());
 Router::use('/api/auth', $routes);
