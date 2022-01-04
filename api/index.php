1cc2bb3ff985ddacba116889a14a426c09abd5f1
Brix-D <Shlyakhta4@gmail.com>
2022-01-04 07:42:01 +0400

    worked with nginx local config

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 use Services\Router;
 use Models\Database;
-require_once __DIR__ . "/vendor/autoload.php";
+require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ . "/config.php";
 
 $db = new Database();
